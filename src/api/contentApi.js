74f2e0685331581a27893b53f19c87dcc234cf8e
Jake Hill <jakereh91@gmail.com>
2021-09-22 12:20:06 +0200

    add suggestions view. Plus added, approving and rejecting

--- src/api/contentApi.js
+++ src/api/contentApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://4b4b-158-248-76-136.ngrok.io", // Enter url of cms server here
+  baseURL: "http://37a2-158-248-76-136.ngrok.io", // Enter url of cms server here
 });
--- src/context/ContentContext.js
+++ src/context/ContentContext.js
@@ -1,5 +1,6 @@
 import createDataContext from "./createDataContext";
 import ContentApi from "../api/contentApi";
+import router from "next/router";
 import Cookies from "js-cookie";
 
 const contentReducer = (state, action) => {
@@ -27,7 +28,24 @@ const getContent = (dispatch) => async (name) => {
   }
 };
 
-const updateContent = (dispatch) => async (name, content) => {
+const createSuggestion = () => async (pageId, suggestions) => {
+  const token = Cookies.get("token");
+  try {
+    await ContentApi.post(
+      `/suggestion/`,
+      { page: pageId, suggestions },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  } catch (error) {
+    console.log("Failed to update!:", error.message);
+  }
+};
+
+const updateContent = (dispatch) => async (name, id, content) => {
   const token = Cookies.get("token");
   try {
     await ContentApi.put(
@@ -39,11 +57,44 @@ const updateContent = (dispatch) => async (name, content) => {
         },
       }
     );
+    await ContentApi.put(
+      `/suggestion/${id}`,
+      { status: "approved" },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
     dispatch({
       type: "save_content",
       payload: { name, content },
     });
+    router.push("/suggestion");
+  } catch (error) {
+    console.log("Failed to update!:", error.message);
+  }
+};
+
+const rejectSuggestion = (dispatch) => async (name, id, content) => {
+  const token = Cookies.get("token");
+  try {
+    await ContentApi.put(
+      `/suggestion/${id}`,
+      { status: "declined" },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    dispatch({
+      type: "save_content",
+      payload: { name, content },
+    });
+    router.push("/suggestion");
   } catch (error) {
     console.log("Failed to update!:", error.message);
   }
@@ -53,7 +104,9 @@ export const { Provider, Context } = createDataContext(
   contentReducer,
   {
     getContent,
+    createSuggestion,
     updateContent,
+    rejectSuggestion,
   },
   {
     Signup: {},
