fd9a90ce327286391da3541443c578edaaa6d499
Jake Hill <jakereh91@gmail.com>
2021-09-29 15:45:36 +0200

    add correct heroku url for api, fix package.json to fix deploy

--- src/api/contentApi.js
+++ src/api/contentApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://37a2-158-248-76-136.ngrok.io", // Enter url of cms server here
+  baseURL: "https://hojaja-cms-server.herokuapp.com", // Enter url of cms server here
 });
